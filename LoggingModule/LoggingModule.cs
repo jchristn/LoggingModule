e6c23390126cc3dd4dd0cba7a1c1403b73320903
Joel Christner <joel.christner@gmail.com>
2016-08-21 10:19:53 -0700

    First commit

---
+++ LoggingModule/LoggingModule.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SyslogLogging
+{
+    public class LoggingModule
+    {
+        #region Public-Enums
+
+        public enum Severity
+        {
+            Debug = 0,
+            Info = 1,
+            Warn = 2,
+            Error = 3,
+            Alert = 4,
+            Critical = 5,
+            Emergency = 6
+        }
+
+        #endregion
+
+        #region Public-Members
+
+        public string SyslogServerIp = "";
+        public int SyslogServerPort = 0;
+        public bool ConsoleEnable = true;
+        public Severity MinimumSeverity;
+        public bool AsyncLogging = false;
+        public bool IncludeUtcTimestamp = true;
+        public bool IncludeSeverity = true;
+        public bool IncludeHostname = true;
+        public bool IncludeThreadId = true;
+        public bool IndentByStackSize = true;
+
+        #endregion
+
+        #region Private-Members
+
+        private UdpClient UDP = null;
+        private string Hostname = null;
+        private object SendLock = new object();
+        private int BaseDepth = 0;
+
+        #endregion
+
+        #region Constructor
+
+        public LoggingModule(
+            string syslogServerIp,
+            int syslogServerPort,
+            bool consoleEnable,
+            Severity minimumSeverity,
+            bool asyncLogging,
+            bool includeUtcTimestamp,
+            bool includeSeverity,
+            bool includeHostname,
+            bool includeThreadId,
+            bool indentByStackSize)
+        {
+            SyslogServerIp = syslogServerIp;
+            SyslogServerPort = syslogServerPort;
+            ConsoleEnable = consoleEnable;
+            MinimumSeverity = minimumSeverity;
+            AsyncLogging = asyncLogging;
+            IncludeUtcTimestamp = includeUtcTimestamp;
+            IncludeSeverity = includeSeverity;
+            IncludeHostname = includeHostname;
+            IncludeThreadId = includeThreadId;
+            IndentByStackSize = indentByStackSize;
+
+            if (!String.IsNullOrEmpty(SyslogServerIp) && SyslogServerPort > 0)
+            {
+                UDP = new UdpClient(SyslogServerIp, SyslogServerPort);
+            }
+            else
+            {
+                UDP = null;
+            }
+
+            Hostname = Dns.GetHostName();
+            StackTrace st = new StackTrace();
+            BaseDepth = st.FrameCount - 1;
+        }
+
+        #endregion
+
+        #region Public-Internal-Classes
+
+        #endregion
+
+        #region Private-Internal-Classes
+
+        #endregion
+
+        #region Public-Methods
+
+        public void Close()
+        {
+            if (UDP != null) UDP.Close();
+        }
+
+        public void Log(Severity sev, string msg)
+        {
+            string message = "";
+            if (IncludeUtcTimestamp) message += DateTime.Now.ToUniversalTime().ToString("MM/dd/yyyy HH:mm:ss") + " ";
+            if (IncludeSeverity) message += sev.ToString() + " ";
+            if (IncludeHostname) message += Hostname + " ";
+            if (IncludeThreadId) message += "thr-" + Thread.CurrentThread.ManagedThreadId + " ";
+            if (IndentByStackSize)
+            {
+                StackTrace st = new StackTrace();
+                int CurrentDepth = st.FrameCount;
+                if (CurrentDepth > BaseDepth)
+                {
+                    for (int i = 0; i < (CurrentDepth - BaseDepth); i++)
+                    {
+                        message += " ";
+                    }
+                }
+            }
+
+            message += msg;
+
+            if (ConsoleEnable)
+            {
+                if (sev >= MinimumSeverity)
+                {
+                    if (!AsyncLogging) SendToConsole(message);
+                    else Task.Run(() => SendToConsole(message));
+                }
+            }
+
+            if (UDP != null)
+            {
+                if (sev >= MinimumSeverity)
+                {
+                    if (!AsyncLogging) SendToSyslog(message);
+                    else Task.Run(() => SendToSyslog(message));
+                }
+            }
+        }
+
+        public void LogException(string module, string method, Exception e)
+        {
+            if (e == null) throw new ArgumentNullException(nameof(e));
+            var st = new StackTrace(e, true);
+            var frame = st.GetFrame(0);
+            int fileLine = frame.GetFileLineNumber();
+            string filename = frame.GetFileName();
+
+            string message =
+                Environment.NewLine +
+                "---" + Environment.NewLine +
+                "An exception was encountered which triggered this message" + Environment.NewLine +
+                "  Module     : " + module + Environment.NewLine +
+                "  Method     : " + method + Environment.NewLine +
+                "  Type       : " + e.GetType().ToString() + Environment.NewLine +
+                "  Data       : " + e.Data + Environment.NewLine +
+                "  Inner      : " + e.InnerException + Environment.NewLine +
+                "  Message    : " + e.Message + Environment.NewLine +
+                "  Source     : " + e.Source + Environment.NewLine +
+                "  StackTrace : " + e.StackTrace + Environment.NewLine +
+                "  Stack      : " + StackToString() + Environment.NewLine +
+                "  Line       : " + fileLine + Environment.NewLine +
+                "  File       : " + filename + Environment.NewLine +
+                "  ToString   : " + e.ToString() + Environment.NewLine +
+                "  Servername : " + Dns.GetHostName() + Environment.NewLine +
+                "---";
+
+            Log(Severity.Alert, message);
+        }
+
+        #endregion
+
+        #region Private-Methods
+
+        private void SendToConsole(string msg)
+        {
+            if (String.IsNullOrEmpty(msg)) return;
+
+            Console.WriteLine(msg);
+        }
+
+        private void SendToSyslog(string msg)
+        {
+            if (String.IsNullOrEmpty(msg)) return;
+
+            if (!String.IsNullOrEmpty(SyslogServerIp)
+                && SyslogServerPort > 0)
+            {
+                if (UDP != null)
+                {
+                    byte[] data = Encoding.UTF8.GetBytes(msg);
+
+                    lock (SendLock)
+                    {
+                        UDP.Send(data, data.Length);
+                    }
+                }
+            }
+        }
+
+        private string StackToString()
+        {
+            string ret = "";
+
+            StackTrace t = new StackTrace();
+            for (int i = 0; i < t.FrameCount; i++)
+            {
+                if (i == 0)
+                {
+                    ret += t.GetFrame(i).GetMethod().Name;
+                }
+                else
+                {
+                    ret += " <= " + t.GetFrame(i).GetMethod().Name;
+                }
+            }
+
+            return ret;
+        }
+
+        private void ConsoleException(string module, string method, Exception e)
+        {
+            if (e == null) throw new ArgumentNullException(nameof(e));
+            var st = new StackTrace(e, true);
+            var frame = st.GetFrame(0);
+            int fileLine = frame.GetFileLineNumber();
+            string filename = frame.GetFileName();
+
+            string message =
+                Environment.NewLine +
+                "---" + Environment.NewLine +
+                "An exception was encountered which triggered this message" + Environment.NewLine +
+                "  Module     : " + module + Environment.NewLine +
+                "  Method     : " + method + Environment.NewLine +
+                "  Type       : " + e.GetType().ToString() + Environment.NewLine +
+                "  Data       : " + e.Data + Environment.NewLine +
+                "  Inner      : " + e.InnerException + Environment.NewLine +
+                "  Message    : " + e.Message + Environment.NewLine +
+                "  Source     : " + e.Source + Environment.NewLine +
+                "  StackTrace : " + e.StackTrace + Environment.NewLine +
+                "  Stack      : " + StackToString() + Environment.NewLine +
+                "  Line       : " + fileLine + Environment.NewLine +
+                "  File       : " + filename + Environment.NewLine +
+                "  ToString   : " + e.ToString() + Environment.NewLine +
+                "  Servername : " + Dns.GetHostName() + Environment.NewLine +
+                "---";
+
+            Console.WriteLine(message);
+        }
+
+        #endregion
+    }
+}
---
+++ LoggingModuleTest/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyslogLogging
+{
+    class Program
+    {
+        public static LoggingModule Logging;
+
+        static void Main(string[] args)
+        {
+            Logging = new LoggingModule("localhost", 514, true, LoggingModule.Severity.Debug, false, true, true, true, true, true);
+            Logging.Log(LoggingModule.Severity.Debug, "Hello from Main!");
+            Method1();
+            Logging.Log(LoggingModule.Severity.Debug, "Back from Method1!  Press ENTER to exit");
+            Console.ReadLine();
+        }
+
+        static void Method1()
+        {
+            Logging.Log(LoggingModule.Severity.Warn, "Warning from Method1!");
+            Method2();
+            Logging.Log(LoggingModule.Severity.Debug, "Back from Method2!");
+        }
+
+        static void Method2()
+        {
+            Logging.Log(LoggingModule.Severity.Alert, "Alert from Method2!");
+            Method3();
+            Logging.Log(LoggingModule.Severity.Debug, "Back from Method3!");
+        }
+
+        static void Method3()
+        {
+            try
+            {
+                Logging.Log(LoggingModule.Severity.Critical, "We're about to get an exception!");
+                Method4(null);
+                Logging.Log(LoggingModule.Severity.Debug, "You shouldn't see me");
+            }
+            catch (Exception e)
+            {
+                Logging.LogException("Program", "Method3", e);
+            }
+        }
+
+        static void Method4(object someArg)
+        {
+            throw new ArgumentNullException(nameof(someArg));
+        }
+    }
+}
